07620e790e2998725a22a2cada769ffc1465bc63
ma <pchengma2828@gmail.com>
2019-06-19 17:08:21 +0800

    1

--- java/Arithematic.java
+++ java/Arithematic.java
@@ -1,9 +1,17 @@
 public class Arithematic {
 
-    public double m_pow(double a, int b) {
+    // By bit-wise operation, or say, the Fourier series
+    double m_pow(double a, int b) {
         double result = 1;
+        if (b == 0) {
+            return 1;
+        }
+        if (b < 0) {
+            a = 1/a;
+            b = -b;
+        }
         while (b > 0) {
-            if ((b & 1) == 1) {
+            if (b % 2 != 0) {
                 result *= a;
             }
             a *= a;
