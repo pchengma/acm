da8fe391551306b09d296876e92e12de7007d3f3
ma <pchengma2828@gmail.com>
2019-07-08 14:29:00 +0800

    1

--- python/Arithematic.py
+++ python/Arithematic.py
@@ -1,4 +1,13 @@
 import math
+import numpy as np
+
+
+def eigen_lin(mat, label=0):
+    if label == 0:
+        vals, cols = np.linalg.eig(mat)
+        return [vals, cols]
+    else:
+        return 0
 
 
 def triangle_function(theta, label=0):
