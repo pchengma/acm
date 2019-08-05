495ca2afd32eb47e2a4b1332f21fedcef380c78f
C <pchengma2828@gmail.com>
2019-08-05 11:03:44 +0800

    Add files via upload

--- java/Sort.java
+++ java/Sort.java
@@ -2,39 +2,66 @@ package exercise;
 
 public class Sort {
 
-	public void mergeArray(int a[], int first, int mid, int last, int temp[]) {
-		int i = first, j = mid + 1;
-		int m = mid, n = last;
-		int k = 0;
-		
-		while (i <= m && j <= n) {
-			if (a[i] <= a[j]) {
-				temp[k++] = a[i++];
-			}
-			else {
-				temp[k++] = a[j++];
-			}
-		}
-		
-		while (i <= m) {
-			temp[k++] = a[i++];
-		}
-		
-		while (j <= n) {
-			temp[k++] = a[j++];
-		}
-		
-		for (i = 0; i < k; i++) {
-			a[first + 1] = temp[i];
-		}
-	}
-	
-	public void mergeSort(int a[], int first, int last, int temp[]) {
-		if (first < last) {
-			int mid = (first + last) / 2;
-			mergeSort(a, first, mid, temp);
-			mergeSort(a, mid + 1, last, temp);
-			mergeArray(a, first, mid, last, temp);
-		}
-	}
+	// Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        
+        /* Create temp arrays */
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+        
+        /*Copy data to temp arrays*/
+        for (int i=0; i < n1; i++) {
+            L[i] = arr[l+i];
+        }
+        
+        for (int j=0; j<n2; j++) {
+            R[j] = arr[m+1+ j];
+        }
+
+        /* Merge the temp arrays */
+        // Initial indexes of first and second subarrays 
+        int i = 0, j = 0;
+  
+        // Initial index of merged subarry array 
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) { 
+                arr[k++] = L[i++];
+            } 
+            else { 
+                arr[k++] = R[j++];
+            } 
+        } 
+  
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) { 
+            arr[k++] = L[i++]; 
+        } 
+  
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) { 
+            arr[k++] = R[j++]; 
+        } 
+    } 
+  
+    // Main function that sorts arr[l..r] using 
+    // merge() 
+    public void mergeSort(int arr[], int l, int r) { 
+        if (l < r) { 
+            // Find the middle point 
+            int m = (l + r) / 2; 
+  
+            // Sort first and second halves 
+            mergeSort(arr, l, m); 
+            mergeSort(arr , m+1, r); 
+  
+            // Merge the sorted halves 
+            merge(arr, l, m, r); 
+        } 
+    } 
 }
