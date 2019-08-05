893fa9795cc46b882fd0b573938e75fd075908bf
C <pchengma2828@gmail.com>
2019-08-05 09:39:32 +0800

    Add files via upload

--- java/Sort.java
+++ java/Sort.java
@@ -2,22 +2,39 @@ package exercise;
 
 public class Sort {
 
-	public void MergeArray(int a[], int n, int b[], int m, int c[]) {
-		int i, j, k;
-		i = j = k = 0;
-		while (i < n && j < m) {
-			if (a[i] < b[j]) {
-				c[k++] = a[i++];
+	public void mergeArray(int a[], int first, int mid, int last, int temp[]) {
+		int i = first, j = mid + 1;
+		int m = mid, n = last;
+		int k = 0;
+		
+		while (i <= m && j <= n) {
+			if (a[i] <= a[j]) {
+				temp[k++] = a[i++];
 			}
 			else {
-				c[k++] = b[j++];
+				temp[k++] = a[j++];
 			}
 		}
-		while (i < n) {
-			c[k++] = a[i++];
+		
+		while (i <= m) {
+			temp[k++] = a[i++];
 		}
-		while (j < m) {
-			c[k++] = b[j++];
+		
+		while (j <= n) {
+			temp[k++] = a[j++];
+		}
+		
+		for (i = 0; i < k; i++) {
+			a[first + 1] = temp[i];
+		}
+	}
+	
+	public void mergeSort(int a[], int first, int last, int temp[]) {
+		if (first < last) {
+			int mid = (first + last) / 2;
+			mergeSort(a, first, mid, temp);
+			mergeSort(a, mid + 1, last, temp);
+			mergeArray(a, first, mid, last, temp);
 		}
 	}
 }
--- java/Test.java
+++ java/Test.java
@@ -3,8 +3,14 @@ package exercise;
 public class Test {
 
 	public static void main(String[] args) {
-		ReadTxt readtxt = new ReadTxt();
+		/*
+		 * ReadTxt readtxt = new ReadTxt();
 		readtxt.read_one_string();
+		*/
 		
+		Sort mergesort = new Sort();
+		int a[] = {1, 2, 3, 4};
+		int temp[] = {1, 2};
+		mergesort.mergeSort(a, 0, 3, temp);
 	}
 }
