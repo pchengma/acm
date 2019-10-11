64017d3069712f16dbc24dc7df0a351499099c52
pchengma <pchengma2828@gmail.com>
2019-10-11 10:04:38 +0800

    Add files via upload

---
+++ java/QOps.java
@@ -0,0 +1,11 @@
+package core.java;
+
+public class QOps {
+	
+    public double[][] q_compress(double[][] mat) {
+        double[][] sum = null;
+
+        return sum;
+    }
+    
+}
---
+++ java/QOpsTest.java
@@ -0,0 +1,10 @@
+package core.java;
+
+public class QOpsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
