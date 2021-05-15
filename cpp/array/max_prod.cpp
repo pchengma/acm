2d4782c75c73262f5b6bc55698d6cf58a1680839
Pengcheng Ma <pcma@cse.cuhk.edu.hk>
2021-05-15 10:59:30 +0800

    Create max_prod.cpp

---
+++ cpp/array/max_prod.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+int maxProduct(vector<int>& nums) {
+    int maxF = nums[0], minF = nums[0], ans = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+        int mx = maxF, mn = minF;
+        maxF = max(mx * nums[i], max(nums[i], mn * nums[i]));
+        minF = min(mn * nums[i], min(nums[i], mx * nums[i]));
+        ans = max(maxF, ans);
+    }
+    return ans;
+}
+
