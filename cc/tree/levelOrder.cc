6692717bc9cd3f32f954656165623501ed955310
pchengma <pchengma.enoch@gmail.com>
2021-12-17 18:11:34 +0800

    Updated daily exercises 20211217.

---
+++ cc/tree/levelOrder.cc
@@ -0,0 +1,45 @@
+// LeetCode: 102. Binary Tree Level Order Traversal (Medium)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector <vector<int>> levelOrder(TreeNode *root) {
+        vector <vector<int>> ans;
+
+        if (!root) {
+            return ans;
+        }
+
+        queue < TreeNode * > q;
+        q.push(root);
+
+        int size;
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> v;
+            while (size--) {
+                auto node = q.front();
+                v.push_back(node->val);
+                q.pop();
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            ans.push_back(v);
+        }
+
+        return ans;
+    }
+};
