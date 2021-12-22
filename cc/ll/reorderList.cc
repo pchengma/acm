a9e96ccaa1219d5a4aa394ec4d23a43ed661c16f
pchengma <pchengma.enoch@gmail.com>
2021-12-22 19:07:50 +0800

    Updated daily exercises 20211222.

---
+++ cc/ll/reorderList.cc
@@ -0,0 +1,50 @@
+// LeetCode: 143. Reorder List (Medium)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode *head) {
+        if (!head) {
+            return;
+        }
+
+        ListNode *tmp = head, *half = head, *prev = NULL;
+        while (tmp->next && tmp->next->next) {
+            tmp = tmp->next->next;
+            half = half->next;
+        }
+
+        if (tmp->next) {
+            half = half->next;
+        }
+
+        while (half) {
+            tmp = half->next;
+            half->next = prev;
+            prev = half;
+            half = tmp;
+        }
+        half = prev;
+
+        while (head && half) {
+            tmp = head->next;
+            prev = half->next;
+            head->next = half;
+            half->next = tmp;
+            head = tmp;
+            half = prev;
+        }
+
+        if (head && head->next) {
+            head->next->next = NULL;
+        }
+    }
+};
