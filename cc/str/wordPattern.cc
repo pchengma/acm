77c38c86bbea258f3a7b09cab216641674ff45c0
pchengma <pchengma.enoch@gmail.com>
2022-01-17 09:55:18 +0800

    Updated daily exercises 20220117.

---
+++ cc/str/wordPattern.cc
@@ -0,0 +1,21 @@
+// LeetCode: 290. Word Pattern (Easy)
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, int> p2i;
+        unordered_map<string, int> w2i;
+        istringstream in(s);
+
+        int i = 0, n = pattern.size();
+
+        for (string word; in >> word; ++i) {
+            if (i == n || p2i[pattern[i]] != w2i[word]) {
+                return false;
+            }
+
+            p2i[pattern[i]] = w2i[word] = i + 1;
+        }
+
+        return i == n;
+    }
+};
