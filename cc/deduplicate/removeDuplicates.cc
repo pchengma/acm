883ff7acee37c54ebfb025d1fb286b5e0f583ca2
pchengma <pchengma.enoch@gmail.com>
2022-02-06 14:50:51 +0800

    Updated daily exercises 20220206.

---
+++ cc/deduplicate/removeDuplicates.cc
@@ -0,0 +1,15 @@
+// LeetCode: 80. Remove Duplicates from Sorted Array II (Medium)
+class Solution {
+public:
+    int removeDuplicates(vector<int> &nums) {
+        int i = 0;
+
+        for (int n: nums) {
+            if (i < 2 || n > nums[i - 2]) {
+                nums[i++] = n;
+            }
+        }
+
+        return i;
+    }
+};
