71fd557d80856c1a131446b16eb15f185b805908
pchengma <pchengma.enoch@gmail.com>
2022-04-01 12:01:31 +0800

    Updated daily exercises 20220401.

---
+++ cc/str/reverseString.cc
@@ -0,0 +1,10 @@
+// LeetCode: 344. Reverse String (Easy)
+class Solution {
+public:
+    void reverseString(vector<char> &s) {
+        for (int i = 0; i < s.size() / 2; ++i) {
+            swap(s[i], s[s.size() - 1 - i]);
+        }
+
+    }
+};
