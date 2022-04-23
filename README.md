e1185d8a729d727c2ab2f2288b8e2539b31b6678
pchengma <pchengma.enoch@gmail.com>
2022-04-23 12:51:20 +0800

    Updated daily exercises 20220423.

--- README.md
+++ README.md
@@ -132,6 +132,7 @@ Learn a new algorithm every day!
 - No. 520. [Detect Capital](cc/str/detectCapitalUse.cc) (Easy)
 - No. 525. [Contiguous Array](cc/array/findMaxLength.h) (Medium)
 - No. 532. [K-diff Pairs in an Array](cc/hash/findPairs.h) (Medium)
+- No. 535. [Encode and Decode TinyURL](cc/str/TinyURL.h) (Medium)
 - No. 538. [Convert BST to Greater Tree](cc/tree/convertBST.cc) (Medium)
 - No. 560. [Subarray Sum Equals K](java/sum/SubarraySum.java) (Medium)
 - No. 563. [Binary Tree Tilt](cc/tree/findTilt.cc) (Easy)
---
+++ cc/str/TinyURL.h
@@ -0,0 +1,21 @@
+// LeetCode: 535. Encode and Decode TinyURL (Medium)
+class TinyURL {
+public:
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));//
+// Created by Pengcheng Ma on 2022/4/23.
+//
+
