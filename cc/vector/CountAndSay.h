422fa782fc307ac432231622939681ad842df595
Pengcheng Ma <pchengma.enoch@gmail.com>
2022-10-18 08:44:52 +0800

    Updated daily exercises 20221018.
    Signed-off-by: Pengcheng Ma <pchengma.enoch@gmail.com>

---
+++ cc/vector/CountAndSay.h
@@ -0,0 +1,33 @@
+// LeetCode: 38. Count and Say (Medium)
+class CountAndSay {
+public:
+    string countAndSay(int n) {
+        vector <string> v;
+        string s = "";
+        v.push_back(s);
+        for (int i = 1; i <= n; i++) {
+            s = v[i - 1];
+            if (s == "") {
+                v.push_back("1");
+            } else {
+                string ans = "";
+                int count = 1, flag = 1;
+                for (int i = 0; i < s.size() - 1; ++i) {
+                    if (s[i + 1] == s[i]) {
+                        count++;
+                    } else {
+                        ans += (count + '0');
+                        ans += s[i];
+                        count = 1;
+                    }
+                }
+                if (flag == 1) {
+                    ans += (count + '0');
+                    ans += s[s.size() - 1];
+                }
+                v.push_back(ans);
+            }
+        }
+        return v[n];
+    }
+};
